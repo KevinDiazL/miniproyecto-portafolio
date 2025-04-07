0aa0cf7de6da106c9bb3c8ab5f219565004e6ba1
Kevin <kevinmanueldiazluis@gmail.com>
2025-04-07 16:19:44 -0700

    Primera etapa

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                dark: "#0D1117",
+                light: "#FFFFFF",
+                highlight: "#E3FF6A",
+                neonGreen: "#A4FF7A",
+                deepBlue: "#1E293B",
+                softGray: "#8892B0",
+            },
+            backgroundImage: {
+                "gradient-main": "linear-gradient(135deg, #1E293B 0%, #0D1117 100%)",
+            },
+        },
+    },
+    plugins: [],
+};
